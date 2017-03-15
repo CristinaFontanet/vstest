8ef174fc6bc195222b0e7e544db429777d835955
Cristina <cfontanet@sfy.com>
2017-03-15 15:59:44 +0100

    csproj port changed

--- vsmacTest/Program.cs
+++ vsmacTest/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace vsmacTest
 {
@@ -12,7 +13,12 @@ namespace vsmacTest
     {
         public static void Main(string[] args)
         {
+			var config = new ConfigurationBuilder()
+				.AddEnvironmentVariables(prefix: "ASPNETCORE_")
+				.Build();
+			
             var host = new WebHostBuilder()
+				.UseConfiguration(config)
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
